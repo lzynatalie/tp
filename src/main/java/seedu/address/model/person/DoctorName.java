93c6a98dc3f30684dae30df2377d899bde75534c
e1384467 <e1384467@u.nus.edu>
2026-03-25 02:58:05 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/model/person/DoctorName.java
+++ src/main/java/seedu/address/model/person/DoctorName.java
@@ -3,8 +3,6 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
-import java.util.Objects;
-
 /**
  * Represents a Doctor in the system.
  * Guarantees: immutable; name is validated and non-null.
